a1cad28decfeeb8076b930f1615a0ed644e88d0c
Doug <27059467+doug-newton@users.noreply.github.com>
2023-11-05 21:09:56 +0200

    refactor mesh generation
    Squashed commit of the following:
    
    commit ee2644d43278ed0f13392cb4eefae1a5c1e0dffa
    Author: Doug <27059467+doug-newton@users.noreply.github.com>
    Date:   Sun Nov 5 21:08:52 2023 +0200
    
        use generateSide in TileMesh
    
    commit 4572d369062aecbae6869a59d4378ca71355fc7f
    Author: Doug <27059467+doug-newton@users.noreply.github.com>
    Date:   Sun Nov 5 21:08:16 2023 +0200
    
        add param to avoid incorrect start pos
    
    commit c00eccf0bb9507b9faaffaf2f9974dc360b62aba
    Author: Doug <27059467+doug-newton@users.noreply.github.com>
    Date:   Sun Nov 5 20:52:22 2023 +0200
    
        move mesh gen functions into mesh_algorithm
    
    commit bab0e8eb042a242664e8b7f6f1f91b1e3f857173
    Author: Doug <27059467+doug-newton@users.noreply.github.com>
    Date:   Sun Nov 5 20:41:30 2023 +0200
    
        rename for clarity
    
    commit 3ce69fdfbf770b1ebf70c56223a0abbd43588eed
    Author: Doug <27059467+doug-newton@users.noreply.github.com>
    Date:   Sun Nov 5 20:38:40 2023 +0200
    
        refactor / extract methods

--- game/meshes/tile_mesh.cpp
+++ game/meshes/tile_mesh.cpp
@@ -20,93 +20,18 @@ float* TileMesh::generateVertices() {
 	int normalOffset = 3;
 	int textureOffset = 6;
 
-	for (int i = 4; i < 5; i++) {
-
-		//	axis and sign will iterate through:
-		//	0,  1,  2,  0,  1,  2
-		//	1, -1,  1, -1,  1, -1
-
-		int axis = i % 3;
-		int sign = 1 - 2 * (i % 2);
-
-		//	determine the other 2 coords to update in each position, 
-		//	i.e. must skip the axis component,
-		//	which is fixed for the current side
-
-		int p1 = 0;
-		while (p1 == axis) p1++;
-		int p2 = p1 + 1;
-		while (p2 == axis) p2++;
-
-		//	determine where to start populating elements (each side = 12 elements)
-
-		int start = 0;
-
-		//	populate the first position of the side
-
-		vertices[positionOffset + start + axis] = (float)sign * 0.5f;
-		vertices[positionOffset + start + p1] = -1 * 0.5f;
-		vertices[positionOffset + start + p2] = -1 * 0.5f;
-
-		//	populate first normal
-
-		vertices[start + normalOffset + axis] = (float)sign * 1.0f;
-
-		for (int currentVertex = 1; currentVertex < 4; currentVertex++) {
-
-			//	-------------------
-			//	set position values
-			//	-------------------
-
-			vertices[positionOffset + start + currentVertex * vertexWidth + axis] = sign * 0.5f;
-
-			// by default, set the current other 2 coords to the previous
-
-			int p1CurrentIndex = positionOffset + start + currentVertex * vertexWidth + p1;
-			int p1LastIndex = positionOffset + start + (currentVertex - 1) * vertexWidth + p1;
-
-			int p2CurrentIndex = positionOffset + start + currentVertex * vertexWidth + p2;
-			int p2LastIndex = positionOffset + start + (currentVertex - 1) * vertexWidth + p2;
-
-			//	then take turns on which coord to flip
-			//	first flip p1, then flip p2, then flip p1
-
-			if (currentVertex % 2 == 1) {
-				vertices[p1CurrentIndex] = -vertices[p1LastIndex];
-				vertices[p2CurrentIndex] = vertices[p2LastIndex];
-			}
-			else {
-				vertices[p1CurrentIndex] = vertices[p1LastIndex];
-				vertices[p2CurrentIndex] = -vertices[p2LastIndex];
-			}
-
-			//	-----------------
-			//	set normal values
-			//	-----------------
-
-			int normalIndex = start + currentVertex * vertexWidth + normalOffset + axis;
-
-			vertices[normalIndex] = (float)sign * 1.0f;
-
-			//	------------------
-			//	set texture values
-			//	------------------
-
-			int t1CurrentIndex = start + currentVertex * vertexWidth + textureOffset + 0;
-			int t2CurrentIndex = start + currentVertex * vertexWidth + textureOffset + 1;
-			int t1LastIndex = start + (currentVertex - 1) * vertexWidth + textureOffset + 0;
-			int t2LastIndex = start + (currentVertex - 1) * vertexWidth + textureOffset + 1;
-
-			if (currentVertex % 2 == 1) {
-				vertices[t1CurrentIndex] = 1 - vertices[t1LastIndex];
-				vertices[t2CurrentIndex] = vertices[t2LastIndex];
-			}
-			else {
-				vertices[t1CurrentIndex] = vertices[t1LastIndex];
-				vertices[t2CurrentIndex] = 1 - vertices[t2LastIndex];
-			}
-		}
-	}
+	generateSide(
+		vertices,
+		numVerticesPerSide,
+		vertexWidth,
+		positionOffset,
+		normalOffset,
+		textureOffset,
+		//	4 = the top side of a cube
+		4,
+		//	only one side - so start of population not determined by side index
+		true
+	);
 
 	return vertices;
 }
