b45689c443a104985db276d42c88f771d90e4e20
Doug <27059467+doug-newton@users.noreply.github.com>
2023-11-05 11:28:25 +0200

    generate cube verticies - points only as ints

--- main.cpp
+++ main.cpp
@@ -1,25 +1,75 @@
 #include<iostream>
 
-void iterate_sides() {
+void generate_cube_vertices() {
+	int numSides = 6;
+	int numVerticesPerSide = 4;
+	int numElementsPerVertex = 3;
+	int dataSize = numSides * numVerticesPerSide * numElementsPerVertex;
+
+	int* vertices = new int[dataSize];
+
+	for (int i = 0; i < dataSize; i++)
+		vertices[i] = 0;
+
 	for (int i = 0; i < 6; i++) {
+
 		int axis = i % 3;
 		int sign = 1 - 2 * (i % 2);
+		//	axis and sign will iterate through:
 		//	0,  1,  2,  0,  1,  2
 		//	1, -1,  1, -1,  1, -1
-		std::cout << "axis=" << axis << " " << sign << std::endl;
+		std::cout << "axis=" << axis << ", sign=" << sign << std::endl;
 
-		int p = 0;
-		while (p == axis) {
-			p++;
+		//	determine the other 2 coords to update in each position, 
+		//	i.e. must skip the axis component,
+		//	which is fixed for the current side
+
+		int p1 = 0;
+		while (p1 == axis) p1++;
+		int p2 = p1 + 1;
+		while (p2 == axis) p2++;
+
+		//	determine where to start populating elements (each side = 12 elements)
+
+		int start = i * 12;
+
+		//	populate the first position of the side
+
+		vertices[start + axis] = sign;
+		vertices[start + p1] = -1;
+		vertices[start + p2] = -1;
+
+		for (int c = 1; c < 4; c++) {
+			vertices[start + c * 3 + axis] = sign;
+
+			// by default, set the current other 2 coords to the previous
+
+			vertices[start + c * 3 + p1] = vertices[start + (c - 1) * 3 + p1];
+			vertices[start + c * 3 + p2] = vertices[start + (c - 1) * 3 + p2];
+
+			//	then take turns on which coord to flip
+			//	first flip p1, then flip p2, then flip p1
+
+			if (c % 2 == 1) {
+				vertices[start + c * 3 + p1] = -vertices[start + c * 3 + p1];
+			}
+			else {
+				vertices[start + c * 3 + p2] = -vertices[start + c * 3 + p2];
+			}
 		}
-		std::cout << "altering " << p << std::endl;
-		int p2 = p + 1;
-		while (p2 == axis) {
-			p2++;
+	}
+
+	for (int side = 0; side < numSides; side++) {
+		for (int pos = 0; pos < numVerticesPerSide; pos++) {
+			for (int comp = 0; comp < numElementsPerVertex; comp++) {
+				std::cout << vertices[side * (numVerticesPerSide * numElementsPerVertex) + pos * numElementsPerVertex + comp] << ", ";
+			}
+			std::cout << std::endl;
 		}
-		std::cout << "altering " << p2 << std::endl;
 		std::cout << std::endl;
 	}
+
+	delete[] vertices;
 }
 
 void iterate_corners() {
@@ -49,7 +99,7 @@ void iterate_corners() {
 
 int main() {
 
-	iterate_sides();
+	generate_cube_vertices();
 
 	return 0;
 }
