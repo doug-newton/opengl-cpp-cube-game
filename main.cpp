f975890d0e3afd6689f76ece2296da6d5cec226b
Doug <27059467+doug-newton@users.noreply.github.com>
2023-11-09 22:40:08 +0200

    read, compile, and link shaders

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
+std::string read_all_lines(const char* source) noexcept(false);
+GLuint create_shader_program();
 
-int main(int argc, char** argv)
-{
+bool checkShaderCompilation(GLuint shaderID) {
+	GLint success;
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+
+	if (success) {
+		return true;
+	}
+
+	GLint infoLogLength;
+	glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+	std::vector<GLchar> infoLog(infoLogLength);
+	glGetShaderInfoLog(shaderID, infoLogLength, nullptr, infoLog.data());
+	std::cerr << "shader compile error:\n" << infoLog.data() << "\n";
+
+	return false;
+}
+
+int main(int argc, char** argv) {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -33,6 +56,8 @@ int main(int argc, char** argv)
 
 	glClearColor(0.5f, 0.25f, 0.0f, 1.0f);
 
+	GLuint program = create_shader_program();
+
 	while (!glfwWindowShouldClose(window)) {
 		process_input(window);
 
@@ -45,6 +70,55 @@ int main(int argc, char** argv)
 	glfwTerminate();
 
 	return 0;
+}
+
+std::string read_all_lines(const char* source) noexcept(false) {
+	std::stringstream ss;
+	std::ifstream file(source);
+
+	if (!file.is_open()) {
+		throw std::string("could not open ") + source;
+	}
+
+	ss << file.rdbuf();
+	file.close();
+	return ss.str();
+}
+
+bool compile_and_attach_shader(GLuint programID, unsigned int type, const char* path) {
+	std::string source = read_all_lines(path);
+	const GLchar* sourceCStr = source.c_str();
+
+	GLuint shaderID = glCreateShader(type);
+	glShaderSource(shaderID, 1, &sourceCStr, NULL);
+	glCompileShader(shaderID);
+
+	if (!checkShaderCompilation(shaderID)) {
+		glDeleteShader(shaderID);
+		std::cerr << std::string("could not compile shader");
+		return false;
+	}
+
+	glAttachShader(programID, shaderID);
+
+	return true;
+}
+
+GLuint create_shader_program() {
+	GLuint programID = glCreateProgram();
+
+	bool status = true;
+	status = status && compile_and_attach_shader(programID, GL_VERTEX_SHADER, "res\\vertex_shader.glsl");
+	status = status && compile_and_attach_shader(programID, GL_FRAGMENT_SHADER, "res\\fragment_shader.glsl");
+
+	if (!status) {
+		glDeleteProgram(programID);
+		return 0;
+	}
+
+	glLinkProgram(programID);
+
+	return programID;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
