05a9331dbde54ad71feca6993d75df0619ec5e91
Doug <27059467+doug-newton@users.noreply.github.com>
2023-11-10 18:59:59 +0200

    create octagon with GL_TRIANGLE_FAN

--- main.cpp
+++ main.cpp
@@ -90,6 +90,50 @@ GLuint createTriangleVao() {
 	return vao;
 }
 
+GLuint createFanVao1() {
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	GLfloat vertices[] = {
+		 0.0f,  1.0f, 1.0f,		1.0f, 1.0f, 1.0f, 0.3f,
+		-0.7f,  0.7f, 1.0f,		1.0f, 0.0f, 1.0f, 0.3f,
+		-1.0f,  0.0f, 1.0f,		1.0f, 1.0f, 1.0f, 0.3f,
+		-0.7f, -0.7f, 1.0f,		1.0f, 1.0f, 1.0f, 0.3f,
+		 0.0f, -1.0f, 1.0f,		1.0f, 1.0f, 1.0f, 0.3f,
+		 0.7f, -0.7f, 1.0f,		0.0f, 1.0f, 1.0f, 0.3f,
+		 1.0f,  0.0f, 1.0f,		1.0f, 1.0f, 1.0f, 0.3f,
+		 0.7f,  0.7f, 1.0f,		1.0f, 1.0f, 1.0f, 0.3f,
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
+
+	return vao;
+}
+
+GLuint createWuadVao2() {
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+
+}
+
 int main(int argc, char** argv) {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -123,6 +167,7 @@ int main(int argc, char** argv) {
 
 	GLuint quadVao1 = createQuadVao1();
 	GLuint triangleVao = createTriangleVao();
+	GLuint fan = createFanVao1();
 
 	glUseProgram(program);
 
@@ -136,6 +181,9 @@ int main(int argc, char** argv) {
 
 		glBindVertexArray(triangleVao);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(fan);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 8);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
