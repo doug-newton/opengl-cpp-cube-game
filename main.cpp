7e8be615cc1a6146059ed383a2f9995510180e13
Doug <27059467+doug-newton@users.noreply.github.com>
2023-11-13 21:21:17 +0200

    create and print identity matrix

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+void print(std::string label, glm::mat4& matrix) {
+	std::cout << label << ":" << std::endl;
+
+	float* p = glm::value_ptr(matrix);
+
+	for (int r = 0; r < 4; r++) {
+		for (int c = 0; c < 4; c++) {
+			int offset = c * 4 + r;
+			std::cout << *(p + offset) << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
 int main(int argc, char** argv) {
+	glm::mat4 matrix(1.0f);
+	print("identity", matrix);
+
 	return 0;
 }
